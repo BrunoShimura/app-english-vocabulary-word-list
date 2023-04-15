f43faa6cc210153a7534fe02ef552813ed057866
Bruno Shimura <bruno_shimura@hotmail.com>
2023-04-15 16:05:53 -0300

    get-new-api

--- words_api.py
+++ words_api.py
@@ -2,15 +2,10 @@ import pandas as pd
 import requests
 
 df = pd.read_csv("vocabulary.csv")
-# print(df["word"][1000])
+print(df["word"][1000])
 
-url = "https://wordsapiv1.p.rapidapi.com/words/.22-caliber/pertainsTo"
+url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + df["word"][1000]
 
-headers = {
-	"X-RapidAPI-Key": "d247523d56msh2df64077db9950cp1a5cebjsnba141debc31e",
-	"X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com"
-}
-
-response = requests.request("GET", url, headers=headers)
+response = requests.request("GET", url)
 
 print(response.text)
