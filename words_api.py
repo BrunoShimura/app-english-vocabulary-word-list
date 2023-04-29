a2ce4086af1fcd9bd0446b3b6d4e1b094383a421
Bruno Shimura <bruno_shimura@hotmail.com>
2023-04-28 21:47:11 -0300

    function- to-add-to-JSON

--- words_api.py
+++ words_api.py
@@ -1,55 +1,50 @@
 import pandas as pd
 import requests
 import json
+
+# function to add to JSON
+def write_json(new_data, filename='data.json'):
+  with open(filename,'r+') as file:
+    # First we load existing data into a dict.
+    file_data = json.load(file)
+    # Join new_data with file_data inside emp_details
+    file_data["words"].append(new_data)
+    # Sets file's current position at offset.
+    file.seek(0)
+    # convert back to json.
+    json.dump(file_data, file, indent = 4)
+
 df = pd.read_csv("vocabulary.csv")
+url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + "office"
+response = requests.request("GET", url)
+data = json.loads(response.text)[0]
 
-
-#url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + "dog"
-
-#response = requests.request("GET", url)
-
-#data = json.loads(response.text)
-
-
-#df_shuffled=df.sample(frac=1).reset_index(drop=True)
-#df_shuffled.to_csv('output.csv')
-#print(df_shuffled)
-
-
-# Opening JSON file
-f = open('example.json')
-  
-# returns JSON object as 
-# a dictionary
-data = json.load(f)
-
-"""
-
-# all definitions len(data['meanings'][0]['definitions'])
-for i in range(0, 2):
-  if data['meanings'][0]['definitions'][i]['definition']:
-    print(data['meanings'][0]['definitions'][i]['definition'])
+all_audio = []
+all_definitions = []
+all_exemples = []
+all_synonyms = []
 
 # mp3 pthonetcs audio
 for i in data['phonetics']:
-  print(i["audio"])
-
-# synonymous
-print(data['meanings'][0]['synonyms'])
-
-"""
-
+  if (i["audio"] != ""):
+    all_audio.append(i["audio"])
 
 # examples
 for i in data['meanings']:
     for j in i['definitions']:
       if len(j) > 3:
-        print("Definition: "+j['definition'])
-        print("Example: "+j['example'])
-        print("\n")
-    print(i['synonyms'])
+        all_definitions.append(j['definition'])
+        all_exemples.append(j['example'])
+    for k in i['synonyms']:
+      all_synonyms.append(k)
 
-# Closing file
-f.close()
-
-
+# python object to be appended
+y = {
+  "id": 1,
+  "definitions": all_definitions,
+  "exemples": all_exemples,
+  "synonyms": all_synonyms,
+  "audio": all_audio,
+}
+     
+write_json(y)
