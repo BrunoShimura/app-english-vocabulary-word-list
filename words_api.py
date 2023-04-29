bbbaad9e606d3c12375d32f458d543acf8d16d56
Bruno Shimura <bruno_shimura@hotmail.com>
2023-04-28 22:09:45 -0300

    finish-script

--- words_api.py
+++ words_api.py
@@ -15,36 +15,45 @@ def write_json(new_data, filename='data.json'):
     json.dump(file_data, file, indent = 4)
 
 df = pd.read_csv("vocabulary.csv")
-url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + "office"
-response = requests.request("GET", url)
-data = json.loads(response.text)[0]
 
-all_audio = []
-all_definitions = []
-all_exemples = []
-all_synonyms = []
+errors = []
 
-# mp3 pthonetcs audio
-for i in data['phonetics']:
-  if (i["audio"] != ""):
-    all_audio.append(i["audio"])
+for w in range(94, len(df)):
+  try:
+    url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + df["word"][w]
+    response = requests.request("GET", url)
+    data = json.loads(response.text)[0]
 
-# examples
-for i in data['meanings']:
-    for j in i['definitions']:
-      if len(j) > 3:
-        all_definitions.append(j['definition'])
-        all_exemples.append(j['example'])
-    for k in i['synonyms']:
-      all_synonyms.append(k)
+    all_audio = []
+    all_definitions = []
+    all_examples = []
+    all_synonyms = []
 
-# python object to be appended
-y = {
-  "id": 1,
-  "definitions": all_definitions,
-  "exemples": all_exemples,
-  "synonyms": all_synonyms,
-  "audio": all_audio,
-}
-     
-write_json(y)
+    # mp3 pthonetcs audio
+    for i in data['phonetics']:
+      if (i["audio"] != ""):
+        all_audio.append(i["audio"])
+
+    # examples
+    for i in data['meanings']:
+        for j in i['definitions']:
+          if len(j) > 3:
+            all_definitions.append(j['definition'])
+            all_examples.append(j['example'])
+        for k in i['synonyms']:
+          all_synonyms.append(k)
+
+    # python object to be appended
+    y = {
+      "id": w,
+      "word": df["word"][w],
+      "definitions": all_definitions,
+      "examples": all_examples,
+      "synonyms": all_synonyms,
+      "audio": all_audio,
+    }
+    print(w)
+        
+    write_json(y)
+  except:
+    errors.append(w)
